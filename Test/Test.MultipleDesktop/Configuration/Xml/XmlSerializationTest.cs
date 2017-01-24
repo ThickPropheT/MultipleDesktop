27e3b37df6c71d2e6e6d5d7d713507e6d63a9c99
Thick_propheT <adamurbas@hotmail.com>
2017-01-23 20:40:59 -0600

    Cleans up unnecessary and confusing test structure. WhenSerializing test class was misleading because the test is of whether the serialized object produces valid XML. It does so by serializing that object. Purposfully denoting this function within a test class name suggests that a test called WhenDeserializing may or should also exist, but this is not the case. Such a test would only serve to test the .Net XmlSerializer.

--- Test/Test.MultipleDesktop/Configuration/Xml/XmlSerializationTest.cs
+++ Test/Test.MultipleDesktop/Configuration/Xml/XmlSerializationTest.cs
@@ -12,101 +12,80 @@ using MultipleDesktop.Mvc.Configuration;
 namespace Test.MultipleDesktop.Configuration.Xml
 {
     [TestClass]
-    public class XmlSerializationTest
+    public abstract class XmlSerializationTest
     {
         public const string Schema0Path = @"Configuration\Xml\schema0.xsd";
         public const string Schema0Namespace = @"";
         public const string Schema1Path = @"Configuration\Xml\schema1.xsd";
         public const string Schema1Namespace = @"http://microsoft.com/wsdl/types/";
 
-        [TestClass]
-        public abstract class WhenSerializing : XmlSerializationTest
+        private static XmlReaderSettings _settings;
+
+        [ClassInitialize]
+        public static void ClassInit(TestContext context)
         {
-            private static XmlReaderSettings _settings;
+            _settings = new XmlReaderSettings { ValidationType = ValidationType.Schema };
+            _settings.Schemas.Add(Schema0Namespace, XmlReader.Create(new StreamReader(Schema0Path)));
+            _settings.Schemas.Add(Schema1Namespace, XmlReader.Create(new StreamReader(Schema1Path)));
+        }
 
-            [ClassInitialize]
-            public static void ClassInit(TestContext context)
+        protected abstract AppConfiguration GetAppConfiguration();
+
+        [TestClass]
+        public sealed class WhenConfigurationIsEmpty : XmlSerializationTest
+        {
+            protected override AppConfiguration GetAppConfiguration()
             {
-                _settings = new XmlReaderSettings { ValidationType = ValidationType.Schema };
-                _settings.Schemas.Add(Schema0Namespace, XmlReader.Create(new StreamReader(Schema0Path)));
-                _settings.Schemas.Add(Schema1Namespace, XmlReader.Create(new StreamReader(Schema1Path)));
+                return new AppConfiguration();
             }
 
-            protected abstract AppConfiguration GetAppConfiguration();
-
-            private void SerializedXmlShouldMatchXsdImpl()
+            [TestMethod]
+            public void SerializedXmlShouldMatchXsd()
             {
-                var serializer = new XmlSerializer(typeof(AppConfiguration));
-
-                using (var stream = new MemoryStream())
-                {
-                    serializer.Serialize(stream, GetAppConfiguration());
-
-                    stream.Position = 0;
-
-                    using (var xmlReader = XmlReader.Create(stream, _settings))
-                    {
-                        // throws when xml does not match xsd
-                        while (xmlReader.Read()) ;
-                    }
-                }
+                SerializedXmlShouldMatchXsdImpl();
             }
+        }
 
+        [TestClass]
+        public abstract class WhenConfigurationHasDesktops : XmlSerializationTest
+        {
             [TestClass]
-            public sealed class WhenConfigurationIsEmpty : WhenSerializing
+            public sealed class WhenDesktopHasSetup : WhenConfigurationHasDesktops
             {
+                public const string AnyString = "MOCK_VALUE";
+                public static readonly string MockPath = $@"Configuration\Xml\{typeof(WhenDesktopHasSetup).FullName}\MOCK_PATH";
+
+                private static IEnumerable<Func<VirtualDesktopConfiguration>> _setups;
+
+                private Func<VirtualDesktopConfiguration> _currentSetup;
+
                 protected override AppConfiguration GetAppConfiguration()
                 {
-                    return new AppConfiguration();
-                }
-
-                [TestMethod]
-                public void SerializedXmlShouldMatchXsd()
-                {
-                    SerializedXmlShouldMatchXsdImpl();
-                }
-            }
-
-            [TestClass]
-            public abstract class WhenConfigurationHasDesktops : WhenSerializing
-            {
-                [TestClass]
-                public sealed class WhenDesktopHasSetup : WhenConfigurationHasDesktops
-                {
-                    public const string AnyString = "MOCK_VALUE";
-                    public static readonly string MockPath = $@"Configuration\Xml\{typeof(WhenDesktopHasSetup).FullName}\MOCK_PATH";
-
-                    private static IEnumerable<Func<VirtualDesktopConfiguration>> _setups;
-
-                    private Func<VirtualDesktopConfiguration> _currentSetup;
-
-                    protected override AppConfiguration GetAppConfiguration()
-                    {
-                        return new AppConfiguration(
-                            new[]
-                            {
-                                _currentSetup()
-                            });
-                    }
-
-                    /// <summary>
-                    /// <see cref="UsingThisClassConfiguration(TestContext) below for list of <see cref="_setups"/>."/>
-                    /// </summary>
-                    [TestMethod]
-                    public void ForEachSetupSerializedXmlShouldMatchXsd()
-                    {
-                        foreach (var setup in _setups)
+                    return new AppConfiguration(
+                        new[]
                         {
-                            _currentSetup = setup;
+                                _currentSetup()
+                        });
+                }
 
-                            SerializedXmlShouldMatchXsdImpl();
-                        }
-                    }
-
-                    [ClassInitialize]
-                    public static void UsingThisClassConfiguration(TestContext context)
+                /// <summary>
+                /// <see cref="UsingThisClassConfiguration(TestContext) below for list of <see cref="_setups"/>."/>
+                /// </summary>
+                [TestMethod]
+                public void ForEachSetupSerializedXmlShouldMatchXsd()
+                {
+                    foreach (var setup in _setups)
                     {
-                        var setups = new List<Func<VirtualDesktopConfiguration>>
+                        _currentSetup = setup;
+
+                        SerializedXmlShouldMatchXsdImpl();
+                    }
+                }
+
+                [ClassInitialize]
+                public static void UsingThisClassConfiguration(TestContext context)
+                {
+                    var setups = new List<Func<VirtualDesktopConfiguration>>
                         {
                             OfDefault,
                             () => WithBackgroundPath(AnyString),
@@ -114,50 +93,67 @@ namespace Test.MultipleDesktop.Configuration.Xml
                             () => WithBackgroundPathThat(MockPath, exists : false)
                         };
 
-                        foreach (Fit fit in Enum.GetValues(typeof(Fit)))
-                        {
-                            setups.Add(() => WithFit(fit));
-                        }
-
-                        _setups = setups;
-                    }
-
-                    private static VirtualDesktopConfiguration OfDefault()
+                    foreach (Fit fit in Enum.GetValues(typeof(Fit)))
                     {
-                        return new VirtualDesktopConfiguration();
+                        setups.Add(() => WithFit(fit));
                     }
 
-                    private static VirtualDesktopConfiguration WithBackgroundPath(FilePath filePath)
+                    _setups = setups;
+                }
+
+                private static VirtualDesktopConfiguration OfDefault()
+                {
+                    return new VirtualDesktopConfiguration();
+                }
+
+                private static VirtualDesktopConfiguration WithBackgroundPath(FilePath filePath)
+                {
+                    return new VirtualDesktopConfiguration { BackgroundPathElement = filePath };
+                }
+
+                private static VirtualDesktopConfiguration WithBackgroundPathThat(FilePath filePath, bool exists)
+                {
+                    var alreadyExists = Directory.Exists(filePath);
+
+                    if (alreadyExists && !exists)
                     {
-                        return new VirtualDesktopConfiguration { BackgroundPathElement = filePath };
-                    }
+                        Directory.Delete(filePath);
 
-                    private static VirtualDesktopConfiguration WithBackgroundPathThat(FilePath filePath, bool exists)
+                        if (Directory.Exists(filePath))
+                            Assert.Inconclusive($"Could not remove directory '{filePath}'.");
+                    }
+                    if (!alreadyExists && exists)
                     {
-                        var alreadyExists = Directory.Exists(filePath);
+                        Directory.CreateDirectory(filePath);
 
-                        if (alreadyExists && !exists)
-                        {
-                            Directory.Delete(filePath);
-
-                            if (Directory.Exists(filePath))
-                                Assert.Inconclusive($"Could not remove directory '{filePath}'.");
-                        }
-                        if (!alreadyExists && exists)
-                        {
-                            Directory.CreateDirectory(filePath);
-
-                            if (!Directory.Exists(filePath))
-                                Assert.Inconclusive($"Could not create directory '{filePath}.");
-                        }
-
-                        return WithBackgroundPath(filePath);
+                        if (!Directory.Exists(filePath))
+                            Assert.Inconclusive($"Could not create directory '{filePath}.");
                     }
 
-                    private static VirtualDesktopConfiguration WithFit(Fit fit)
-                    {
-                        return new VirtualDesktopConfiguration { FitElement = fit };
-                    }
+                    return WithBackgroundPath(filePath);
+                }
+
+                private static VirtualDesktopConfiguration WithFit(Fit fit)
+                {
+                    return new VirtualDesktopConfiguration { FitElement = fit };
+                }
+            }
+        }
+
+        private void SerializedXmlShouldMatchXsdImpl()
+        {
+            var serializer = new XmlSerializer(typeof(AppConfiguration));
+
+            using (var stream = new MemoryStream())
+            {
+                serializer.Serialize(stream, GetAppConfiguration());
+
+                stream.Position = 0;
+
+                using (var xmlReader = XmlReader.Create(stream, _settings))
+                {
+                    // throws when xml does not match xsd
+                    while (xmlReader.Read()) ;
                 }
             }
         }
