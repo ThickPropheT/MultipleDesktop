66fcf07871d4623c063513d59ecaa17f7f0ca052
Thick_propheT <adamurbas@hotmail.com>
2017-01-23 23:33:22 -0600

    Adds Exception<T>(Action) to Expect for when the expected exception isn't thrown from a mock.

--- Test/VisualStudio.TestTools.UnitTesting/Expect.cs
+++ Test/VisualStudio.TestTools.UnitTesting/Expect.cs
@@ -13,14 +13,33 @@ namespace VisualStudio.TestTools.UnitTesting
 
                 Assert.Fail($"Expected exception '{e}'");
             }
-            catch (AssertFailedException ex)
+            catch (AssertFailedException)
             {
-                throw ex;
+                throw;
             }
             catch (Exception ex)
             {
                 if (!ReferenceEquals(ex, e))
                     Assert.Fail($"Expected exception '{e}', but encountered '{ex}'");
+            }
+        }
+
+        public static void Exception<T>(Action fromScope) where T : Exception
+        {
+            try
+            {
+                fromScope();
+
+                Assert.Fail($"Expected exception of type '{typeof(T).FullName}'");
+            }
+            catch (AssertFailedException)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                if (!(ex is T))
+                    Assert.Fail($"Expected exception of type '{typeof(T).FullName}', but encountered '{ex}'");
             }
         }
     }
