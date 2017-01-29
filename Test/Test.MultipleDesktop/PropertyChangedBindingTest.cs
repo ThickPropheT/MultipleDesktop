4d1723454134c05b01bfd9015377debffc87790a
Thick_propheT <adamurbas@hotmail.com>
2017-01-28 19:57:18 -0600

    Adds tests for PropertyChangedBinding.

---
+++ Test/Test.MultipleDesktop/PropertyChangedBindingTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.ComponentModel;
+using Moq;
+using MultipleDesktop.Mvc;
+using Should.Fluent;
+
+namespace Test.MultipleDesktop
+{
+    [TestClass]
+    public class PropertyChangedBindingTest
+    {
+        public const string AnyString = "MOCK_VALUE";
+
+        [TestClass]
+        public sealed class WhenPropertyChanges : PropertyChangedBindingTest
+        {
+            [TestMethod]
+            public void ShouldExecuteCallback()
+            {
+                var sourceMock = new Mock<INotifyPropertyChanged>();
+
+                bool invoked = false;
+                Action action = () => invoked = true;
+
+                var binding = new PropertyChangedBinding(sourceMock.Object, action);
+
+                sourceMock.Raise(source => source.PropertyChanged += null, sourceMock.Object, new PropertyChangedEventArgs(AnyString));
+
+                invoked.Should().Be.True();
+            }
+        }
+
+        [TestClass]
+        public sealed class WhenUnbound : PropertyChangedBindingTest
+        {
+            [TestMethod]
+            public void ShouldNotExecuteCallback()
+            {
+                var sourceMock = new Mock<INotifyPropertyChanged>();
+
+                bool invoked = false;
+                Action action = () => invoked = true;
+
+                var binding = new PropertyChangedBinding(sourceMock.Object, action);
+                binding.Unbind();
+
+                sourceMock.Raise(source => source.PropertyChanged += null, sourceMock.Object, new PropertyChangedEventArgs(AnyString));
+
+                invoked.Should().Be.False();
+            }
+        }
+    }
+}
