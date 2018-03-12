90a759bc62e48f65bf7ec37f68dcfca65961e855
Thick_propheT <adamurbas@hotmail.com>
2018-03-12 16:46:29 -0500

    Arranges project according to function (Application, View). Adds CompositionRoot to act as a View/ViewModel bootstrapper in favor of the default App.xaml StartupUri technique. IMPORTANT: See warning notes about this approach in App.xaml.cs. Applies the MainViewModel to the MainWindow.DataContext as a test view model.

---
+++ MultipleDesktop.Windows.PresentationFoundation/Application/CompositionRoot.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MultipleDesktop.Windows.PresentationFoundation.Application
+{
+    public class MainViewModel
+    {
+        public string Title => "Main Title";
+    }
+
+    public static class CompositionRoot
+    {
+        public static Window Compose()
+        {
+            var window = new MainWindow();
+            window.DataContext = new MainViewModel();
+
+            return window;
+        }
+    }
+}
