41f049f8e57892ea896b33462e20440f2dadd915
Thick_propheT <adamurbas@hotmail.com>
2018-03-16 01:49:19 -0500

    Adds helper method to invoke a delegate if a boolean condition is true and return the condition.

---
+++ System/mscorlib.Extended/System/Invoke.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace System.Extended
+{
+    public static class Invoke
+    {
+        public static bool Delegate(Action @delegate, bool @if)
+        {
+            if (@if) @delegate();
+            return @if;
+        }
+    }
+}
