0b7ca51f6132c9dd42e1b456324d060c2123e681
Thick_propheT <adamurbas@hotmail.com>
2018-03-16 01:46:25 -0500

    Adds extension/helper methods to replace existing "Mediator" pattern classes that unsubscribe event handlers after the first invocation.

---
+++ System/mscorlib.Extended/System.ComponentModel/Event.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+
+namespace System.ComponentModel.Extended
+{
+    public static class Event
+    {
+        public delegate void Subscriber(PropertyChangedEventHandler handler);
+        public delegate void Subscriber<T>(T target, PropertyChangedEventHandler handler);
+        public delegate bool Handler(object sender, PropertyChangedEventArgs e);
+
+        public static void HandleOnce<T>(this T t, Subscriber subscribe, Subscriber unsubscribe, Handler doHandle)
+            where T : INotifyPropertyChanged
+            => HandleOnce(subscribe, unsubscribe, doHandle);
+
+        public static void HandleOnce<T>(this T t, Subscriber<T> subscribe, Subscriber<T> unsubscribe, Handler doHandle)
+            where T : INotifyPropertyChanged
+            => HandleOnce(
+                e => subscribe(t, e),
+                e => unsubscribe(t, e),
+                doHandle);
+
+        public static void HandleOnce(Subscriber subscribe, Subscriber unsubscribe, Handler doHandle)
+        {
+            subscribe(
+                new UnsubscribingHandler(
+                    (thisHandler, s, e) =>
+                    {
+                        if (doHandle(s, e))
+                            unsubscribe(thisHandler);
+                    })
+                .UnsubscribeThenInvoke);
+        }
+
+        private delegate void UnsubscribingHandler(PropertyChangedEventHandler invokedHandler, object sender, PropertyChangedEventArgs e);
+
+        private static void UnsubscribeThenInvoke(this UnsubscribingHandler unsubscribingHandler, object sender, PropertyChangedEventArgs e)
+        {
+            unsubscribingHandler(unsubscribingHandler.UnsubscribeThenInvoke, sender, e);
+        }
+    }
+}
---
+++ System/mscorlib.Extended/System/Event.cs
@@ -0,0 +1,36 @@
+﻿namespace System.Extended
+{
+    public static class Event
+    {
+        public delegate void Subscriber(EventHandler handler);
+        public delegate void Subscriber<T>(T target, EventHandler handler);
+
+        public static void HandleOnce<T>(this T t, Subscriber subscribe, Subscriber unsubscribe, EventHandler doHandle)
+            => HandleOnce(subscribe, unsubscribe, doHandle);
+
+        public static void HandleOnce<T>(this T t, Subscriber<T> subscribe, Subscriber<T> unsubscribe, EventHandler doHandle)
+            => HandleOnce(
+                e => subscribe(t, e),
+                e => unsubscribe(t, e),
+                doHandle);
+
+        public static void HandleOnce(Subscriber subscribe, Subscriber unsubscribe, EventHandler doHandle)
+        {
+            subscribe(
+                new UnsubscribingHandler(
+                    (thisHandler, s, e) =>
+                    {
+                        unsubscribe(thisHandler);
+                        doHandle(s, e);
+                    })
+                .UnsubscribeThenInvoke);
+        }
+
+        private delegate void UnsubscribingHandler(EventHandler invokedHandler, object sender, EventArgs e);
+
+        private static void UnsubscribeThenInvoke(this UnsubscribingHandler unsubscribingHandler, object sender, EventArgs e)
+        {
+            unsubscribingHandler(unsubscribingHandler.UnsubscribeThenInvoke, sender, e);
+        }
+    }
+}
